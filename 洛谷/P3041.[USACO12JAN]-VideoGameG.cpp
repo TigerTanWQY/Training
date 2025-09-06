346f9c0ac6ff52852b930a5e847b1b090e04447d
TigerTanWQY <TatingWan@outlook.com>
2025-09-07 00:23:32 +0800

    2025-09-07-01

---
+++ 洛谷/P3041.[USACO12JAN]-VideoGameG.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#define re register int
+using namespace std;
+
+ int read()
+{
+  int x=0,f=1;char ch=getchar();
+  while(!isdigit(ch)&&ch!='-')ch=getchar();
+  if(ch=='-')f=-1,ch=getchar();
+  while(isdigit(ch))x=(x<<1)+(x<<3)+ch-'0',ch=getchar();
+  return x*f;
+}
+ void print(int x)
+{
+  if(x<0)putchar('-'),x=-x;
+  if(x/10)print(x/10);
+  putchar(x%10+'0');
+}
+int n,k,ch[400][3],v[400],f[1002][400],fail[400],cnt,ans;
+char s[20];
+ void insert()
+{
+	scanf("%s",s+1);int len=strlen(s+1),u=0;
+	for(re i=1;i<=len;i++)
+	{
+		int x=s[i]-'A';
+		if(!ch[u][x])ch[u][x]=++cnt;
+		u=ch[u][x];
+	}
+	v[u]++;
+}
+ void get_fail()
+{
+	queue<int>q;
+	for(re i=0;i<3;i++)if(ch[0][i])q.push(ch[0][i]);
+	while(!q.empty())
+	{
+		int u=q.front();q.pop();
+		for(re i=0;i<3;i++)
+		{
+			if(ch[u][i])fail[ch[u][i]]=ch[fail[u]][i],q.push(ch[u][i]);
+			else ch[u][i]=ch[fail[u]][i];
+		}
+		v[u]+=v[fail[u]];
+	}
+}
+ void dp()
+{
+	memset(f,128,sizeof(f)); //here
+	for(re i=0;i<=k;i++)f[i][0]=0;
+	for(re i=1;i<=k;i++)
+	for(re j=0;j<=cnt;j++)
+	for(re l=0;l<3;l++)
+	f[i][ch[j][l]]=max(f[i][ch[j][l]],f[i-1][j]+v[ch[j][l]]);
+}
+signed main()
+{
+	n=read();k=read();
+	for(re i=1;i<=n;i++)insert();
+	get_fail();dp();
+	for(re i=1;i<=cnt;i++)ans=max(ans,f[k][i]);
+	printf("%d",ans);
+	return 0;
+}
--- 洛谷/P5829.【模板】失配树.cpp
+++ 洛谷/P5829.【模板】失配树.cpp
@@ -1,9 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr const int N = 1e6 + 3;
+vector<int> G[N];
+bool vis[N];
+char s[N];
+int kmp[N], h[N], dep[N], fa[24][N];
+
+void dfs(int u) {
+	dep[u] = dep[fa[0][u]] + 1;
+	for(int i = 1; i <= 21; ++i)
+		fa[i][u] = fa[i-1][fa[i-1][u]];
+	for(const auto& v: G[u]) {
+		if(v == fa[0][u])
+			continue;
+		fa[0][v] = u;
+		dfs(v);
+	}
+}
+int LCA(int u, int v) {
+	if(dep[u] < dep[v])
+		swap(u, v);
+	for(int i = 21; ~i; --i)
+		if(dep[fa[i][u]] >= dep[v])
+			u = fa[i][u];
+	if(u == v)
+		return u;
+	for(int i = 21; ~i; --i)
+		if(fa[i][u] != fa[i][v]) {
+			u = fa[i][u];
+			v = fa[i][v];
+		}
+	return fa[0][u];
+}
+
 int main() {
 	cin.tie(nullptr)->sync_with_stdio(false);
-	;
-	cout << endl;
-	return 0;
+	cin >> s;
+	const int n = strlen(s);
+	for(int i = n; i; --i)
+		swap(s[i], s[i-1]);
+	for (int i = 2, j = 0; i <= n; ++i) {
+		while(j && s[i] != s[j+1])
+			j = kmp[j];
+		if(s[i] == s[j+1])
+			++j;
+		kmp[i] = j;
+	}
+	for(int i = n; i; --i)
+		if(!vis[i]) {
+			vis[i] = true;
+			for(int j = i; ; ) {
+				if(!j)
+					break;
+				G[j].push_back(kmp[j]);
+				G[kmp[j]].push_back(j);
+				j = kmp[j];
+				if(vis[j])
+					break;
+				vis[j] = true;
+			}
+		}
+	fa[0][0] = n + 1;
+	dfs(0);
+	int _T;
+	cin >> _T;
+	for(int u, v; _T--; cout.put('\n')) {
+		cin >> u >> v;
+		int k = LCA(u, v);
+		if(k == u || k == v)
+			k = fa[0][k];
+		cout << k;
+	}
+	cout.flush(); return 0;
 }
