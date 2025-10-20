4197fb2fe0c68019ef6b4cf386a17864644f6a8e
TigerTanWQY <TatingWan@outlook.com>
2025-10-20 23:27:22 +0800

    2025-10-20-01

--- 洛谷/P3825.[NOI2017]-游戏.cpp
+++ 洛谷/P3825.[NOI2017]-游戏.cpp
@@ -1,14 +1,103 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr const int N = 2e5 + 3;
+int n, d, m, a1[N], b1[N], ecnt, nxt[N], adj[N], go[N], dfn[N], low[N],
+times, num, bel[N], top, stk[N], cyx[N];
+char s[N], a2[N], b2[N], orz[N];
+bool ins[N], flg;
+
+void add_edge(int u, int v) {
+	nxt[++ecnt] = adj[u]; adj[u] = ecnt; go[ecnt] = v;
+}
+int neg(int x) {return x > n? x - n: x + n;}
+int tran(int x, char c) {
+	if (s[x] == 'a') return c == 'B'? x: x + n;
+	if (s[x] == 'b' || s[x] == 'c') return c == 'A'? x: x + n;
+	if (c == 'C') return x + n; return x;
+}
+void Tarjan(int u) {
+	dfn[u] = low[u] = ++times; ins[stk[++top] = u] = 1;
+	for (int e = adj[u], v; e; e = nxt[e])
+		if (!dfn[v = go[e]]) {
+			Tarjan(v);
+			low[u] = min(low[u], low[v]);
+		}
+		else if (ins[v]) low[u] = min(low[u], dfn[v]);
+	if (dfn[u] == low[u]) {
+		int v; bel[u] = ++num; ins[u] = 0;
+		while (v = stk[top--], v != u) bel[v] = num, ins[v] = 0;
+	}
+}
+bool solve() {
+	int i; ecnt = times = num = 0;
+	for (i = 1; i <= (n << 1); i++) bel[i] = adj[i] = dfn[i] = 0;
+	for (i = 1; i <= m; i++)
+		if (s[a1[i]] != 'x' && s[b1[i]] != 'x') {
+			if (a2[i] == s[a1[i]] - 32) continue;
+			int u = tran(a1[i], a2[i]), v;
+			if (b2[i] == s[b1[i]] - 32) {add_edge(u, neg(u)); continue;}
+			v = tran(b1[i], b2[i]); add_edge(u, v);
+			add_edge(neg(v), neg(u));
+		} else {
+			char o = s[a1[i]], p = s[b1[i]];
+			int u, v, x = cyx[a1[i]], y = cyx[b1[i]];
+			if (o == 'x' && p == 'x') {
+				if (a2[i] == orz[x]) continue;
+				u = tran(a1[i], a2[i]), v;
+				if (b2[i] == orz[y]) {add_edge(u, neg(u)); continue;}
+				v = tran(b1[i], b2[i]); add_edge(u, v);
+				add_edge(neg(v), neg(u));
+			} else if (o == 'x' && p != 'x') {
+				if (a2[i] == orz[x]) continue;
+				u = tran(a1[i], a2[i]), v;
+				if (b2[i] == s[b1[i]] - 32) {add_edge(u, neg(u)); continue;}
+				v = tran(b1[i], b2[i]); add_edge(u, v);
+				add_edge(neg(v), neg(u));
+			} else {
+				if (a2[i] == s[a1[i]] - 32) continue;
+				u = tran(a1[i], a2[i]), v;
+				if (b2[i] == orz[y]) {add_edge(u, neg(u)); continue;}
+				v = tran(b1[i], b2[i]); add_edge(u, v);
+				add_edge(neg(v), neg(u));
+			}
+		}
+	for (i = 1; i <= (n << 1); i++) if (!dfn[i]) Tarjan(i);
+	for (i = 1; i <= n; i++) if (bel[i] == bel[i + n]) return 0;
+	for (i = 1; i <= n; i++)
+		if (bel[i] < bel[i + n]) {
+			if (s[i] == 'a') cout.put('B');
+			else if (s[i] == 'b' || s[i] == 'C') cout.put('A');
+			else if (orz[cyx[i]] == 'A') cout.put('B');
+			else cout.put('A');
+		} else {
+			if (s[i] == 'a' || s[i] == 'b') cout.put('C');
+			else if (s[i] == 'c') cout.put('B');
+			else if (orz[cyx[i]] == 'A') cout.put('C');
+			else cout.put('B');
+		}
+	return 1;
+}
+void dfs(int dep) {
+	if (dep > d) {
+		if (!flg) flg = solve();
+		if (flg) exit(0);
+		return;
+	}
+	orz[dep] = 'A'; dfs(dep + 1);
+	orz[dep] = 'B'; dfs(dep + 1);
+}
+
 int main() {
 	cin.tie(nullptr)->sync_with_stdio(false);
-	int n, d, m; string s;
-	cin >> n >> d >> s >> m;
-	for(int i, j; m--; ) {
-		char hi, hj;
-		cin >> i >> hi >> j >> hj;
-	}
-	cout << "ABA" << endl;
-	return 0;
+	int i;
+	cin >> n >> i >> s >> m;
+	for (i = n; i; --i)
+		swap(s[i], s[i-1]);
+	for (i = 1; i <= n; i++) if (s[i] == 'x') cyx[i] = ++d;
+	for (i = 1; i <= m; i++)
+		cin >> a1[i] >> a2[i] >> b1[i] >> b2[i];
+	dfs(1);
+	if (!flg) cout << "-1";
+	cout << endl; return 0;
 }
--- 洛谷/P6965.[NEERC2016]-BinaryCode.cpp
+++ 洛谷/P6965.[NEERC2016]-BinaryCode.cpp
@@ -17,7 +17,7 @@ int trie_cnt=1;
 vector<int>G[MAXN<<1];
 int ist(int len, char *s){
 	int x = 1;
-	for( int i=1; i<=len; i++){
+	for(int i=1; i<=len; i++){
 		if(!son(x,s[i]^48)) son(x,s[i]^48) = ++trie_cnt, fa(son(x,s[i]^48)) = x;
 		x = son(x,s[i]^48);
 	}
@@ -31,8 +31,8 @@ char ch[MAXN], *a[MAXN];
 int n, node_cnt, unk[MAXN], loc[2][MAXN], len[MAXN];
 
 void build1(){
-	for( int i=1; i<=trie_cnt; i++) if(fa(i)) Add_Edge(i+node_cnt,fa(i)+node_cnt);
-	for( int i=1, x; i<=n; i++){
+	for(int i=1; i<=trie_cnt; i++) if(fa(i)) Add_Edge(i+node_cnt,fa(i)+node_cnt);
+	for(int i=1, x; i<=n; i++){
 		x = loc[0][i]; if(x) Add_Edge(x+node_cnt,Y(i)); if(fa(x)) Add_Edge(N(i),fa(x)+node_cnt);
 		x = loc[1][i]; if(x) Add_Edge(x+node_cnt,N(i)); if(fa(x)) Add_Edge(Y(i),fa(x)+node_cnt);
 	}
@@ -40,8 +40,8 @@ void build1(){
 }
 
 void build2(){
-	for( int i=1; i<=trie_cnt; i++) if(fa(i)) Add_Edge(fa(i)+node_cnt,i+node_cnt);
-	for( int i=1, x; i<=n; i++){
+	for(int i=1; i<=trie_cnt; i++) if(fa(i)) Add_Edge(fa(i)+node_cnt,i+node_cnt);
+	for(int i=1, x; i<=n; i++){
 		x = loc[0][i]; if(x) Add_Edge(N(i),x+node_cnt); if(fa(x)) Add_Edge(fa(x)+node_cnt,Y(i));
 		x = loc[1][i]; if(x) Add_Edge(Y(i),x+node_cnt); if(fa(x)) Add_Edge(fa(x)+node_cnt,N(i));
 	}
@@ -50,14 +50,14 @@ void build2(){
 
 void build3(){
 	int tmp[MAXN], num;
-	for( int i=1; i<=trie_cnt; i++){
+	for(int i=1; i<=trie_cnt; i++){
 		num = G[i].size();
 		if(num>=2){
-			for( int j=1; j<=num; j++)
+			for(int j=1; j<=num; j++)
 				tmp[j] = G[i][j-1],
 				Add_Edge(tmp[j],j+node_cnt),
 				Add_Edge(j+num+node_cnt,tmp[j]^1);
-			for( int j=1; j<num; j++)
+			for(int j=1; j<num; j++)
 				Add_Edge(j+node_cnt,j+1+node_cnt),
 				Add_Edge(j+node_cnt,tmp[j+1]^1),
 				Add_Edge(tmp[j+1],j+num+node_cnt),
@@ -72,7 +72,7 @@ char in[MAXP];
 void tarjan(int x){
 	q[++top] = x; in[x] = true;
 	dfn[x] = low[x] = ++cnt;
-	for( int u=fst[x]; u; u=edge[u].nxt){
+	for(int u=fst[x]; u; u=edge[u].nxt){
 		int v=edge[u].to;
 		if(!dfn[v]) tarjan(v), low[x] = min(low[x],low[v]);
 		else if(in[v]) low[x] = min(low[x],dfn[v]);
@@ -90,10 +90,10 @@ void tarjan(int x){
 int main(){
 	a[1] = ch;
 	scanf("%d",&n); node_cnt = Y(n);
-	for( int i=1; i<=n; i++){
+	for(int i=1; i<=n; i++){
 		scanf("%s",a[i]+1);
 		len[i] = strlen(a[i]+1);
-		for( int j=1; j<=len[i]; j++) if(a[i][j]=='?') unk[i] = j;
+		for(int j=1; j<=len[i]; j++) if(a[i][j]=='?') unk[i] = j;
 		if(!unk[i]){
 			unk[i] = 1;
 			if(a[i][1]=='0') Add_Edge(Y(i),N(i)), loc[0][i] = ist(len[i],a[i]), G[loc[0][i]].push_back(N(i));
@@ -105,10 +105,10 @@ int main(){
 		a[i+1] = a[i]+len[i];
 	}
 	build1(); build2(); build3();
-	for( int i=1; i<=node_cnt; i++) if(!dfn[i]) tarjan(i);
-	for( int i=1; i<=n; i++) if(bel[N(i)] == bel[Y(i)]) return puts("NO"), 0;
+	for(int i=1; i<=node_cnt; i++) if(!dfn[i]) tarjan(i);
+	for(int i=1; i<=n; i++) if(bel[N(i)] == bel[Y(i)]) return puts("NO"), 0;
 	puts("YES");
-	for( int i=1; i<=n; i++) if(bel[N(i)] < bel[Y(i)]) a[i][unk[i]] = '0'; else a[i][unk[i]] = '1';
-	for( int i=1; i<=n; i++,putchar('\n')) for( int j=1; j<=len[i]; j++) putchar(a[i][j]);
+	for(int i=1; i<=n; i++) if(bel[N(i)] < bel[Y(i)]) a[i][unk[i]] = '0'; else a[i][unk[i]] = '1';
+	for(int i=1; i<=n; i++,putchar('\n')) for( int j=1; j<=len[i]; j++) putchar(a[i][j]);
 	return 0;
 }
