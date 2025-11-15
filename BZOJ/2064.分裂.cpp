41aee4f49219bc1143be19941de81db53079e7a0
TigerTanWQY <TatingWan@outlook.com>
2025-11-15 23:25:09 +0800

    2025-11-15-2

--- BZOJ/2064.分裂.cpp
+++ BZOJ/2064.分裂.cpp
@@ -1,31 +1,27 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-constexpr const int N = 20 + 1;
-int s[1 << N], f[1 << N];
+int s[(1<<20)+3], f[(1<<20)+3];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(nullptr)->sync_with_stdio(false);
 	int n, m;
 	cin >> n;
-	for(int i = 1; i <= n; ++i)
-		cin >> s[1 << i - 1];
+	for(int i = 0; i < n; ++i)
+		cin >> s[1 << i];
 	cin >> m;
-	for(int i = 1, x = 1 << n; i <= m; ++i, x <<= 1) {
+	for(int i = 1, x = 1<<n; i <= m; ++i, x <<= 1) {
 		cin >> s[x];
 		s[x] = -s[x];
 	}
-	int t = (1 << n + m) - 1;
-	for(int i = 1; i <= t; ++i) {
+	int T = (1 << n+m) - 1;
+	for(int i = 1; i <= T; ++i) {
 		s[i] = s[i & -i] + s[i ^ (i & -i)];
-		for(int j = 1; j <= n + m; ++j)
-			if(i & 1 << j - 1)
-				f[i] = max(f[i], f[i - (1 << j - 1)]);
+		for(int j = 0; j < n + m; ++j)
+			if(i >> j & 1)
+				f[i] = max(f[i], f[i - (1 << j)]);
 		f[i] += !s[i];
 	}
-	cout << n + m - f[t] * 2;
-	cout.flush();
+	cout << n + m - f[T] * 2 << endl;
 	return 0;
 }
--- Codeforces/79/D.cpp
+++ Codeforces/79/D.cpp
@@ -1,16 +1,12 @@
-#include <iostream>
-#include <cstring>
-#include <bitset>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-constexpr const int N = 10'003 * 10, M = 10 + 1, K = 103;
-bitset<N> a;
+constexpr const int N = 1e5, M = 11, K = 103;
+bool a[N];
 int b[N], q[N * 2][3], dis[K * 2][N], Q[N], f[N * K];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(nullptr)->sync_with_stdio(false);
 	int n, m, k;
 	cin >> n >> m >> k;
 	for(int i = 1, x; i <= m; ++i) {
@@ -34,33 +30,30 @@ int main() {
 		dis[q[i][0]][q[i][1]] = 0;
 		int L = 1, R = 1;
 		Q[1] = q[i][1];
-		while(L <= R) {
+		for(; L <= R; ++L) {
 			int u = Q[L];
 			for(int j = 1; j <= k; ++j) {
-				if(u - b[j] >= 0 && dis[q[i][0]][u] + 1 < dis[q[i][0]][u - b[j]]) {
+				if(u-b[j] >= 0 && dis[q[i][0]][u] + 1 < dis[q[i][0]][u-b[j]]) {
 					dis[q[i][0]][u-b[j]] = dis[q[i][0]][u] + 1;
 					Q[++R] = u - b[j];
 				}
-				if(u + b[j] <= n && dis[q[i][0]][u] + 1 < dis[q[i][0]][u + b[j]]) {
-					dis[q[i][0]][u + b[j]] = dis[q[i][0]][u] + 1;
+				if(u+b[j] <= n && dis[q[i][0]][u] + 1 < dis[q[i][0]][u+b[j]]) {
+					dis[q[i][0]][u+b[j]] = dis[q[i][0]][u] + 1;
 					Q[++R] = u + b[j];
 				}
 			}
-			++L;
 		}
 	}
 	memset(f, 0x3f, sizeof(f));
 	f[0] = 0;
-	int maxn = (1 << tot) - 1;
+	int maxn = (1<<tot) - 1;
 	for(int i = 0; i <= maxn; ++i) {
 		int x = 0;
-		while(i >> x & 1)
-			++x;
-		for(int y = x + 1; y < tot; ++y)
+		for(; i >> x & 1; ++x);
+		for(int y = x+1; y < tot; ++y)
 			if(!(i >> y & 1))
 				f[i | (1 << x) | (1 << y)] = min(f[i | (1 << x) | (1 << y)], f[i] + dis[x][q[y][1]]);
 	}
-	cout << (f[maxn] == 0x3f3f3f3f? -1: f[maxn]);
-	cout.flush();
+	cout << (f[maxn] == 0x3f3f3f3f? -1: f[maxn]) << endl;
 	return 0;
 }
