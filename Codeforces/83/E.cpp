90126fc20234389053f0326690a013b61fda555a
TigerTanWQY <TatingWan@outlook.com>
2025-11-16 22:32:30 +0800

    2025-11-16-1

--- Codeforces/83/E.cpp
+++ Codeforces/83/E.cpp
@@ -1,44 +1,42 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-constexpr const int N = 200'003, K = 20 + 1;
-char s[N][K];
-int f[1 << K], len;
+constexpr const int N = 2e5+3;
+char s[N][23];
+int f[(1<<21)+3], len;
 
 void reverse(char* const s, const int l) {
 	for(int i = 1, j = l; i < j; ++i, --j)
 		swap(s[i], s[j]);
 }
 
-int getMin(char* const s, const int l) {
-	int now = 1;
+int Min(char* const s, const int l) {
+	int nw = 1;
 	for(int i = 1; i <= l; ++i) {
-		now <<= 1;
-		now |= (s[i] == '0');
+		nw <<= 1;
+		nw |= (s[i] == '0');
 	}
-	return f[now];
+	return f[nw];
 }
 
-int query(char* const s) {
-	int res = 0x3f3f3f3f;
-	for (int i = 0; i <= len; ++i) {
+int qry(char* const s) {
+	int res = 1e9;
+	for(int i = 0; i <= len; ++i) {
 		reverse(s, i);
-		res = min(res, getMin(s, i) + len - i);
+		res = min(res, Min(s, i) + len - i);
 		reverse(s, i);
 	}
 	return res;
 }
 
-int Solve(const char* const s, const char* const t) {
+int solve(const char* const s, const char* const t) {
 	int res = 0;
 	for(int i = len; i; --i) {
-		bool flag = true;
-		for(int j = len; flag && j > len - i; --j)
+		bool flg = true;
+		for(int j = len; flg && j > len - i; --j)
 			if(s[j] != t[i - (len - j)])
-				flag = false;
-		if(flag) {
+				flg = false;
+		if(flg) {
 			res = i;
 			break;
 		}
@@ -46,35 +44,36 @@ int Solve(const char* const s, const char* const t) {
 	return len - res;
 }
 
-void Insert(const char* const s, const int val) {
-	int now = 1;
+void ist(const char* const s, const int w) {
+	int nw = 1;
 	for(int i = 1; i <= len; ++i) {
-		f[now] = min(f[now], val);
-		now <<= 1;
-		now |= (s[i] == '0');
+		f[nw] = min(f[nw], w);
+		nw <<= 1;
+		nw |= (s[i] == '0');
 	}
-	f[now] = min(f[now], val);
+	f[nw] = min(f[nw], w);
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(nullptr)->sync_with_stdio(false);
 	int n;
 	cin >> n;
-	for(int i = 1; i <= n; ++i)
-		cin >> (s[i] + 1);
+	for(int i = 1; i <= n; ++i) {
+		cin >> s[i];
+		for(int j = strlen(s[i]); j; --j)
+			swap(s[i][j], s[i][j-1]);
+	}
 	len = strlen(s[1] + 1);
 	int tag = len;
 	memset(f, 0x3f, sizeof(f));
 	f[1] = 0;
 	for(int i = 2; i <= n; ++i) {
-		int res = query(s[i]);
+		int res = qry(s[i]);
 		res += tag;
-		tag += Solve(s[i - 1], s[i]);
+		tag += solve(s[i - 1], s[i]);
 		reverse(s[i - 1], len);
-		Insert(s[i - 1], res - tag);
+		ist(s[i - 1], res - tag);
 	}
-	cout << f[1] + tag;
-	cout.flush();
+	cout << f[1] + tag << endl;
 	return 0;
 }
--- Codeforces/acmsguru/99999/222.cpp
+++ Codeforces/acmsguru/99999/222.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
-
-constexpr const int N = 10 + 1;
-long long f[N];
+// When your dreams come alive, you're unstoppable.
+long long f[11];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(nullptr)->sync_with_stdio(false);
 	f[0] = f[1] = 1;
-	for(int i = 2; i < N; ++i)
-		f[i] = f[i - 1] * i;
+	for(int i = 2; i < 11; ++i)
+		f[i] = f[i-1] * i;
 	int n, k;
 	cin >> n >> k;
 	if(n < k)
-		cout << '0';
+		cout.put('0');
 	else
-		cout << f[k] * (f[n] / f[k] / f[n - k]) * (f[n] / f[k] / f[n - k]);
-	cout.flush();
-	return 0;
+		cout << f[k] * (f[n] / f[k] / f[n-k]) * (f[n] / f[k] / f[n-k]);
+	cout << endl; return 0;
 }
